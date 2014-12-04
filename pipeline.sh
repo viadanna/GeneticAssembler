c20dc65f4041f1707a52ab542a7528955c4f9a10
Paulo Viadanna <paulo@scylla.com.br>
2014-12-04 17:06:56 -0200

    Better structure for repository

--- pipeline.sh
+++ pipeline.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-cat genome.fasta | python sequencer.py - -o reads.fasta
-cat reads.fasta | julia grapher.jl > reads.edges
-cat reads.edges | python greedy.py -
+echo "Splitting Reference Genome into Reads"
+cat data/genome.fasta | python src/sequencer.py - -o reads.fasta
+echo "Building Overlap Graph edges from Reads"
+cat reads.fasta | julia src/grapher.jl > reads.edges
+echo "Building Hamiltonian path from OG"
+cat reads.edges | python src/greedy.py -
