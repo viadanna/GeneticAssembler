b365806a6917bbb4702cc5c2c588f3afc25767ec
Paulo Viadanna <paulo@scylla.com.br>
2014-12-11 10:44:08 -0200

    Improved code readability of genetic.py

--- src/genetic.py
+++ src/genetic.py
@@ -11,13 +11,22 @@ import sys
 
 
 def genetic(edges, father, mother, random_index=2):
+    """
+    Builds hamiltonian path for the given weigthed edges
+    Will choose a random edge in given list and parents if given
+    Returns the list of vertices, weight and edges not added for lack of a
+    possible path
+    """
     vertices = len(set([e[0] for e in edges]))
     starting = choice(edges[0:random_index])
     results, weight = starting[0:2], starting[2]
-    while True:
+
+    while len(results) < vertices:
+        # Loop until all vertices are added
         vertice = results[-1]
         possible = [e for e in [father.get(vertice)] + [mother.get(vertice)] if e]
         if len(possible) < random_index:
+            # Add more edges if father and mother not possible or enough
             for e in edges:
                 if e[0] == vertice and e[1] not in results:
                     possible.append(e)
@@ -28,17 +37,21 @@ def genetic(edges, father, mother, random_index=2):
             return results, weight, [e for e in edges
                                      if e[0] not in results
                                      and e[1] not in results]
+        # Choose a random edges from possible ones
         index = min([random_index, len(possible)])
         edge = choice(possible[:index])
         results.append(edge[1])
         weight += edge[2]
 
-        if len(results) == vertices:
-            # Full genome assembled
-            return results, weight, None
+    # Full genome assembled
+    return results, weight, None
 
 
 def assemble(edges, father, mother, random_index=2):
+    """
+    Runs the assembly algorithm until all edges are added
+    Returns contigs and the sum of all contigs' weights
+    """
     paths = []
     while edges is not None:  # Assemble all contigs
         path, weight, edges = genetic(
@@ -49,6 +62,7 @@ def assemble(edges, father, mother, random_index=2):
 
 
 def parents_dict(parents):
+    " Turns parent's list of vertices into a dictionary of edges "
     if len(parents) == 0:
         return {}, {}
     father = {v: parents[i+1] for i, v in enumerate(parents[0][0][:-1])}
@@ -93,9 +107,11 @@ if __name__ == '__main__':
 
     father, mother, results = {}, {}, []
     for gen in xrange(args.generations):
-        children = [assemble(given_edges[:], father, mother)
-                    for _ in xrange(args.children)]
-        [verb("Generation %d child: %d\n" % (gen, c[1])) for c in children]
+        children = []
+        for _ in xrange(args.children):
+            child = assemble(given_edges[:], father, mother)
+            children.append(child)
+            verb("Generation %d child: %d\n" % (gen, child[1]))
         children.sort(key=itemgetter(1), reverse=True)
         father, mother = parents_dict(children)
         results += children
